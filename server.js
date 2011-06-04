6c4c5fc4e5fc53398fc0643de8c0ec4dbf6089d8
Matthew Wastrodowski <towski@gmail.com>
2011-06-03 20:44:19 -0700

    stuff

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (towski) is running..');
-}).listen(10331);
+var mongodb = require('mongodb')
+var fs = require('fs')
+var mongo_config;
+
+if(process.env.NODE_ENV == "local"){
+  mongo_config = JSON.parse(fs.readFileSync('config.json.local')).mongo;
+}else{
+  mongo_config = JSON.parse(fs.readFileSync('config.json')).mongo;
+}
+
+mongodb.connect(mongo_config, function(error, db){
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\nApp (towski) is running..');
+  }).listen(10331);
+});
